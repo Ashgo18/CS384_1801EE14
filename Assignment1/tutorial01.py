89c23225aa9f4eae7c0a8f1c3a53d76722ca0a6b
Ashwin Goyal <ashwingoyal805@gmail.com>
2020-09-16 18:11:46 +0530

    My first Basic Python Calci Done.

---
+++ Assignment1/tutorial01.py
@@ -0,0 +1,24 @@
+# Function to add two numbers 
+def add(num1, num2): 
+	#Addition Logic
+	addition = num1 + num2
+	return addition
+
+# Function to subtract two numbers 
+def subtract(num1, num2):
+	#Substraction Logic 
+	subtraction = num1 - num2
+	return subtraction
+
+# Function to multiply two numbers 
+def multiply(num1, num2): 
+	#Multiplication Logic
+	multiplication = num1*num2 
+	return multiplication
+
+# Function to divide two numbers 
+def divide(num1, num2): 
+	#DivisionLogic 
+	division = num1/num2
+	return division
+	
