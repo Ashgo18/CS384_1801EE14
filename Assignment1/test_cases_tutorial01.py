c75a6e58ca626a79c0af3d752d05afb1317c8917
Ashwin Goyal <ashwingoyal805@gmail.com>
2020-09-17 13:54:07 +0530

    Added 4 new functions from Skeleton

--- Assignment1/test_cases_tutorial01.py
+++ Assignment1/test_cases_tutorial01.py
@@ -25,4 +25,5 @@ for x, y in zip(actual_answers, student_answers):
     if x == y:
         count_of_correct_test_cases += 1
 
-print(f"Test Cases Passed = '{count_of_correct_test_cases}'  / '{total_test_cases}'")
+print(
+    f"Test Cases Passed = '{count_of_correct_test_cases}'  / '{total_test_cases}'")
--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -1,24 +1,53 @@
-# Function to add two numbers 
-def add(num1, num2): 
-	#Addition Logic
-	addition = num1 + num2
-	return addition
+# Function to add two numbers
+def add(num1, num2):
+    # Addition Logic
+    addition = num1 + num2
+    return addition
 
-# Function to subtract two numbers 
+# Function to subtract two numbers
+
+
 def subtract(num1, num2):
-	#Substraction Logic 
-	subtraction = num1 - num2
-	return subtraction
+    # Substraction Logic
+    subtraction = num1 - num2
+    return subtraction
 
-# Function to multiply two numbers 
-def multiply(num1, num2): 
-	#Multiplication Logic
-	multiplication = num1*num2 
-	return multiplication
+# Function to multiply two numbers
 
-# Function to divide two numbers 
-def divide(num1, num2): 
+
+def multiply(num1, num2):
+    # Multiplication Logic
+    multiplication = num1*num2
+    return multiplication
+
+# Function to divide two numbers
+
+
+def divide(num1, num2):
+    # DivisionLogic
+    division = num1/num2
+    return division
+
+# Function to add power function
+#You cant use the inbuilt python function x ** y . Write your own function
+def power(num1, num2): #num1 ^ num2
 	#DivisionLogic 
-	division = num1/num2
-	return division
+	return power
 	
+# Python 3 program to print GP.  geometric Progression
+#You cant use the inbuilt python function. Write your own function
+def printGP(a, r, n): 
+	gp=[]
+	return gp 
+
+# Python 3 program to print AP.  arithmetic Progression
+#You cant use the inbuilt python function. Write your own function
+def printAP(a, d, n): 
+	ap=[]
+	return ap
+
+# Python 3 program to print HP.   Harmonic Progression
+#You cant use the inbuilt python function. Write your own function
+def printHP(a, d, n): 
+	hp=[]
+	return hp
