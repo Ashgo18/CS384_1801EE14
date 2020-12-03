d983556601f42d626814c6b4459bd893a573e9e1
Ashwin Goyal <ashwingoyal805@gmail.com>
2020-12-03 13:48:48 +0530

    Renamed the Assignment folders as per guidelines

--- Assignments/Assignment_2/test_cases_tutorial02.py
+++ Assignments/Assignment_2/test_cases_tutorial02.py
@@ -1,6 +1,6 @@
 import tutorial02 as A2
 
-actual_answers = [0.558, 0.641, 0.302, 0.091, -0.389, 1.997, 0.475, 0.226, 0.404, -1.476, 0.051, 0, 0, 0]
+actual_answers = [0.55, 0.64, 0.30, 0.09, -0.38, 1.99, 0.47, 0.22, 0.40, -1.47, 0.05, 0, 0, 0]
 student_answers = []
 
 import numpy as np
@@ -62,7 +62,7 @@ total_test_cases = len(actual_answers)
 count_of_correct_test_cases = 0
 
 for x, y in zip(actual_answers, student_answers):
-    if x == round(y,3):
+    if x == y:
         count_of_correct_test_cases += 1
 
 print(f"Test Cases Passed = '{count_of_correct_test_cases}'  / '{total_test_cases}'")
--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -7,23 +7,24 @@ import shutil
 os.system("cls")
 
 cd = os.getcwd()
-cd = os.path.join(cd,'groups')
+cd = os.path.join(cd, 'groups')
 
 if os.path.isdir(cd):
     shutil.rmtree(cd)
 
 os.mkdir(cd)
 
+
 def group_allocation(filename, number_of_groups):
     # Entire Logic
 
     # Sorting given file i.e FILENAME
     temp_file = 'tempdata.csv'
-    data = pd.read_csv(filename) 
-    data.set_index('Roll',inplace=True)
-    data.sort_values(["Roll"], axis=0,ascending=True, inplace=True)
+    data = pd.read_csv(filename)
+    data.set_index('Roll', inplace=True)
+    data.sort_values(["Roll"], axis=0, ascending=True, inplace=True)
     data.to_csv(temp_file)
-    
+
     branch_strength_header = ['BRANCH_CODE', 'STRENGTH']
     branch_header = ['Name', 'Roll', 'Email']
     stats_grouping_header = ['Group', 'Total']
@@ -46,10 +47,10 @@ def group_allocation(filename, number_of_groups):
                 branch_total[branch] = 0
 
             branch_total[branch] += 1
-            
+
             # Creating branch files
-            curr_file = os.path.join(cd ,branch + '.csv')
-            
+            curr_file = os.path.join(cd, branch + '.csv')
+
             if not os.path.isfile(curr_file):
                 with open(curr_file, 'w', newline='') as file:
                     data = csv.writer(file)
@@ -65,7 +66,8 @@ def group_allocation(filename, number_of_groups):
         branch_total = dict(branch_total)
         branch_total_tups = branch_total.items()
 
-        with open('branch_strength.csv', 'w', newline='') as file:
+        cd1 = os.path.join(cd,'branch_strength.csv')
+        with open(cd1, 'w', newline='') as file:
             data = csv.writer(file)
             data.writerow(branch_strength_header)
             for i in branch_total_tups:
@@ -78,10 +80,10 @@ def group_allocation(filename, number_of_groups):
         for key in branch_total:
             stats_grouping_header.append(key)
 
-        for i in range(1,number_of_groups+1):
+        for i in range(1, number_of_groups+1):
             groups_total.append(0)
-            
-            if i < 10 :
+
+            if i < 10:
                 group_names.append("Group_G0" + str(i))
             else:
                 group_names.append("Group_G" + str(i))
@@ -98,18 +100,19 @@ def group_allocation(filename, number_of_groups):
 
             for j in range(number_of_groups):
                 temp.append(stud_per_group)
-                groups_stats_info[1][j]+=stud_per_group
+                groups_stats_info[1][j] += stud_per_group
 
             while remaining_stud:
-                temp[present_group]+=1
-                groups_stats_info[1][present_group]+=1
-                remaining_stud-=1
-                present_group+=1
-                present_group%=number_of_groups
-            
+                temp[present_group] += 1
+                groups_stats_info[1][present_group] += 1
+                remaining_stud -= 1
+                present_group += 1
+                present_group %= number_of_groups
+
             groups_stats_info.append(temp)
 
-        with open('stats_grouping.csv','w',newline='') as file:
+        cd1 = os.path.join(cd,'stats_grouping.csv')
+        with open(cd1 , 'w', newline='') as file:
             data = csv.writer(file)
             data.writerow(stats_grouping_header)
 
@@ -118,39 +121,42 @@ def group_allocation(filename, number_of_groups):
 
                 for j in range(len(groups_stats_info)):
                     temp.append(groups_stats_info[j][i])
-                
+
                 data.writerow(temp)
-        
-        #Creating Individual Groups
+
+        # Creating Individual Groups
         temp = 1
-        
+
         for key in branch_total:
-            cdd = os.path.join(cd,key+'.csv')
-            with open(cdd,'r') as file:
+            cdd = os.path.join(cd, key+'.csv')
+            with open(cdd, 'r') as file:
                 data = csv.reader(file)
-                data_list = list(data) 
+                data_list = list(data)
                 del data_list[0]
-                
+
                 z = 0
                 x = 0
                 temp += 1
                 for i in range(number_of_groups):
-                    curr_file = os.path.join(cd,groups_stats_info[0][i]+'.csv')
+                    curr_file = os.path.join(
+                        cd, groups_stats_info[0][i]+'.csv')
                     z += groups_stats_info[temp][i]
 
                     if not os.path.isfile(curr_file):
-                        with open(curr_file,'w',newline='') as file:
+                        with open(curr_file, 'w', newline='') as file:
                             data = csv.writer(file)
                             data.writerow(branch_header)
-                    
-                    with open(curr_file,'a+',newline='') as file:
+
+                    with open(curr_file, 'a+', newline='') as file:
                         data = csv.writer(file)
                         while x < z:
                             data.writerow(data_list[x])
-                            x+=1
+                            x += 1
 
     os.remove(temp_file)
 
+
 filename = "Btech_2020_master_data.csv"
-number_of_groups = int(input("Enter the number of groups in which students have to be divided:- "))
+number_of_groups = int(
+    input("Enter the number of groups in which students have to be divided:- "))
 group_allocation(filename, number_of_groups)
